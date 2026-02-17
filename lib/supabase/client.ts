3acac28522fefec79e0b9e0c49917d18e5951f14
Massimiliano Masi <spiridione@hotmail.com>
2026-02-17 07:24:30 +0400

    new anon

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://pkwbzhucmhfaoljugmgs.supabase.co";
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrd2J6aHVjbWhmYW9sanVnbWdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUwNDMxMjAsImV4cCI6MjA1MDYxOTEyMH0.XLQ5F8bBqmqGM4WxzqiPc1sTiGhxhsQJHkb0ozDi7WQ";
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrd2J6aHVjbWhmYW9sanVnbWdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNDI4ODYsImV4cCI6MjA4NjgxODg4Nn0.P7Zg2_yS-h5fNY8mAi2TydW_jZtT6hDSZq_JJhwKfv8";
 
 export function createClient() {
   return createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
